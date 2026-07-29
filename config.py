0ca06dda7f4ebd392e618743e6d53637e797a67a
AshutoshCode-xy <ashutosh230611@gmail.com>
2026-07-29 17:01:38 +0530

    Add configuration loading from environment variables

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+VERIFY_TOKEN = os.getenv("VERIFY_TOKEN")
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+PHONE_NUMBER_ID = os.getenv("PHONE_NUMBER_ID")
+
+PORT = os.getenv("PORT",5000)
